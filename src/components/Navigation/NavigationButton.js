8b97f22272d94ba6f63bbc624b706a00e04db081
Hariom <harim987316@gmail.com>
2022-01-01 13:50:36 +0530

    Adding styles

--- src/components/Navigation/NavigationButton.js
+++ src/components/Navigation/NavigationButton.js
@@ -1,11 +1,16 @@
 // importing files
-import React from "react";
+import React, { useState } from "react";
 import "./NavigationButton.css"; // css file
 
 // functionality start from here
 const NavButton = (props) => {
+  const [flag, setflag] = useState(true);
   const _click = (event) => {
     console.warn(`|>_ ${event.target.innerHTML} is clicked`);
+    if (window.innerWidth < 700) {
+      document.getElementsByClassName("navigation-overlay")[0].style.width =
+        "0%";
+    }
     activeButton(event.target.id);
     props.setdata(event.target.innerHTML);
   };
@@ -17,6 +22,13 @@ const NavButton = (props) => {
     let current = document.getElementById(id);
     current.classList = "navigation-button active";
   };
+  setTimeout(() => {
+    if (flag) {
+      let _active = document.getElementsByClassName("navigation-button")[0];
+      _active.className = "navigation-button active";
+      setflag(false);
+    }
+  }, 0);
   return (
     <button id={props.id} className="navigation-button" onClick={_click}>
       {props.notes}
