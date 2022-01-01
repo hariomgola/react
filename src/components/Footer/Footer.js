342a679400110e58af7905e361aa412ae4e6a44f
Hariom <harim987316@gmail.com>
2022-01-01 14:05:18 +0530

    change css on vw basis

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+// importing libraries
+import React from "react";
+
+// functionality start from here
+const Footer = () => {
+  return (
+    <div>
+      <br></br>
+    </div>
+  );
+};
+
+// exporting functionality
+export default Footer;
